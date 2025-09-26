d274de79e3cb785de90d500ad6dfbf119c44fef2
SamanthaToet <samanthactoet@gmail.com>
2025-09-26 11:40:31 -0400

    download and tidy school climate data

--- scripts/school_climate.R
+++ scripts/school_climate.R
@@ -26,17 +26,66 @@ climate_23 <- climate_23_raw %>%
   select(-state_id, -state_name) %>%
   mutate(
     across(
-    .cols = matches("q"),
+    .cols = matches("q") | matches("id"),
     .fns = ~ as.numeric(gsub("%", "", .))),
     s_num = as.numeric(s_num),
     yr = 2023)
 
-# Drop state average and save as separate dataframe:
+# Create state average dataframe:
 climate_state_23 <- climate_23 %>%
-  filter(region_id == "State Average") %>%
-  distinct(region_id, .keep_all = T)
+  filter(division_name == "State Average") %>%
+  distinct(division_name, .keep_all = T) %>%
+  select(-matches("id|name"))
 
-# Drop district averages and save and separate dataframe 
+# Create division average dataframe:
+climate_div_23 <- climate_23 %>%
+  filter(school_name == "Division Average") %>%
+  select(-matches("school"))
+
+# Create school-level dataframe:
+climate_sch_23 <- climate_23 %>%
+  filter(!school_name %in% c("State Average", "Division Average"))
+
+# 2022 ----
+# Download: 
+box_fetch(dir_id = 320085027632, local_dir = "data/raw/school_climate/2022/")
+
+# Read student data:
+files_22 <- list.files("data/raw/school_climate/2022", pattern = "\\.xlsx", full.names = TRUE)
+climate_22_raw <- map_dfr(files_22, ~read_excel(.x, sheet = "Data_student"))
+
+# Tidy:
+climate_22 <- climate_22_raw %>%
+  distinct() %>%
+  clean_names() %>%
+  select(-state_id, -state_name) %>%
+  rename(s_num = student_num) %>%
+  mutate(
+    across(
+      .cols = matches("stu") | matches("id"),
+      .fns = ~ as.numeric(gsub("%", "", .))),
+    s_num = as.numeric(s_num),
+    yr = 2022,
+    region_name = gsub(".*-\\s*", "", region_name))
+
+# Create state average dataframe:
+climate_state_22 <- climate_22 %>%
+  filter(region_name == "State Average") %>%
+  select(-matches("id|name"))
+
+# Create division average dataframe:
+climate_div_22 <- climate_22 %>%
+  filter(school_name == "Division Average") %>%
+  select(-matches("school"))
+
+# Create region average dataframe:
+climate_reg_22 <- climate_22 %>%
+  filter(school_name == "Region Average") %>%
+  select(-matches("district|division|school"))
+
+# Create school-level dataframe:
+climate_sch_22 <- climate_22 %>%
+  filter(!grepl("Average", school_name))
 
 
 
