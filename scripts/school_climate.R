8b774e7b5f7b3a1bfe5d994c39960f9c36ba22aa
SamanthaToet <samanthactoet@gmail.com>
2026-02-22 18:35:17 -0500

    fix missing regional 2023 measures

--- scripts/school_climate.R
+++ scripts/school_climate.R
@@ -81,6 +81,41 @@ climate_22 <- rename_climate_data(climate_22, rename_map_22)
 missing <- climate_23 %>%
   filter(is.na(region_id))
 
+climate <- bind_rows(climate_22, climate_23)
+
+# Add region name:
+vdoe_regions <- read_csv("data/vdoe_regions_divisions.csv") %>%
+  select(region_name, region_number) %>%
+  distinct()
+
+# Add locality grouping (for easier filtering): ----
+climate <- climate %>%
+  left_join(vdoe_regions, by = join_by(region_id == region_number)) 
+
+## State
+climate_state <- climate %>%
+  filter(school_name == "State Average") %>%
+  distinct() %>%
+  mutate(division_name = NA,
+         school_name = NA,
+         locality_grouping = "state") 
+
+## Division 
+climate_div <- climate %>%
+  filter(school_name == "Division Average") %>%
+  mutate(school_name = NA,
+         locality_grouping = "division")
+
+## School 
+climate_sch <- climate %>%
+  filter(!school_name %in% c("Division Average", "Region Average", "State Average")) %>%
+  mutate(locality_grouping = "school")
+
+## Region  22
+climate_reg <- climate %>%
+  filter(school_name == "Region Average") %>%
+  mutate(locality_grouping = "region")
+
 # Calculate region averages for 2023 (this is provided in the 2022 data, but not in the 2023 summary)
 reg_23 <- climate_23 %>%
   group_by(region_id) %>%
@@ -91,28 +126,25 @@ reg_23 <- climate_23 %>%
       .fns = ~mean(.x, na.rm = T)
     )) %>%
   filter(!is.na(region_id)) %>%
-  mutate(locality_grouping = "region")
+  mutate(locality_grouping = "region",
+         yr = 2023)
 
-# Combine & Save ----
-climate <- bind_rows(climate_22, climate_23, reg_23)
-
-# Add region name:
-vdoe_regions <- read_csv("data/vdoe_regions_divisions.csv") %>%
-  select(region_name, region_number) %>%
-  distinct()
-
-# Add locality grouping (for easier filtering):
-climate <- climate %>%
-  left_join(vdoe_regions, by = join_by(region_id == region_number)) %>%
-  mutate(locality_grouping = case_when(
-    division_name == "Region Average" ~ "region",
-    division_name == "State Average" ~ "state",
-    school_name == "Division Average" ~ "division",
-    !is.na(school_id) ~ "school")) %>%
-    distinct()
+reg_23 <- left_join(reg_23, vdoe_regions, by = c("region_id" = "region_number"))
 
 # Note that some of the regions provided are not used in other places (EX. 9-12, 999)
 # so they come up as having NA region names 
 
+# Combine & Save ----
+
+climate_new <- bind_rows(reg_23, climate_reg, climate_div, climate_sch, climate_state) %>%
+  select(-region_id, -state_name, -district_id, -school_id) %>%
+  mutate(
+    division_name = case_when(
+      division_name == "Region Average" ~ NA,
+      TRUE ~ division_name),
+    school_name = case_when(
+      school_name == "Region Average" ~ NA,
+      TRUE ~ school_name))
+
 # Save:
-write_csv(climate, "data/climate.csv")
+write_csv(climate_new, "data/climate.csv")
